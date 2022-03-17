f765bcd77a7523d8193dcf372bf5ad178bf9330a
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2022-03-17 23:49:31 +0530

    Change

--- 1. Js Introduction/2. ArrayMap/1_ArrayMapDemo.js
+++ 1. Js Introduction/2. ArrayMap/1_ArrayMapDemo.js
@@ -1,10 +1,10 @@
 
-// node 1_ArraysMapDemo.js fffff
+// node 1_ArraysMapDemo.js
 // Map is itself a fn
 // Map takes as input a callback fn (with v and i)
 // map will call the callback multiple times (once for each value)
 // for each run of callback, map will pass v and i to callback
-// callback will process the value and index and return a single value
+// callback will process the value and inde x and return a single value
 // Single value returned by each run of callback will be collected in a new array
 // Map returns that new array
 
